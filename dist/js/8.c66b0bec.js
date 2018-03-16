f0262416f2a0caf1bb5aa8169738c09dc8dcb504
nano <shinanome@qq.com>
2018-03-16 15:49:00 +0800

    add demo

---
+++ dist/js/8.c66b0bec.js
@@ -0,0 +1,2 @@
+webpackJsonp([8],{"0z3G":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});t("n12u");var i=t("NYxO"),a={name:"dashboard",computed:Object.assign({},Object(i["b"])({adminTitle:function(e){return e.adminConfig.title}}))},r=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{attrs:{id:"dashboard"}},[e._v("\n  欢迎进入"+e._s(e.adminTitle)+"\n")])},u=[],c=t("XyMi");function d(e){t("hfj6")}var o=!1,s=d,l=null,f=null,b=Object(c["a"])(a,r,u,o,s,l,f);n["default"]=b.exports},hfj6:function(e,n){}});
+//# sourceMappingURL=8.c66b0bec.js.map
